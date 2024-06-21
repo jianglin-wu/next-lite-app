32344cffd5e9ef31fa590ca48ed29c0ca158a101
jianglin-wu <wjiangl.id@gmail.com>
2024-06-21 17:10:53 +0800

    chore: test ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By Lt
+            By Lt2
           </a>
         </div>
       </div>
